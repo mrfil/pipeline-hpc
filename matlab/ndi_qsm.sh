8236184cb696b1c5a52dee27c3cdd994e7e9ed8e
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2021-09-22 08:12:29 -0500

    Updated Quantitative Susceptibility Mapping f threshold argument

--- matlab/ndi_qsm.sh
+++ matlab/ndi_qsm.sh
@@ -5,7 +5,7 @@
 # bind directories for data and toolboxes on run, then specify their paths in this script
 # bind working directory for functions
 #
-# singularity run --bind /local/path/to/ndi:/ndi,/path/to/IMAs:/datain,scripts/matlab:/scripts matlab-R2019a.sif /scripts/ndi_qsm.sh 
+# singularity run --bind /local/path/to/ndi:/ndi,/path/to/IMAs:/datain,scripts/matlab:/scripts matlab-R2019a.sif /scripts/ndi_qsm.sh ${intensity} 
+intensity=$1
 
-
-matlab -nodisplay -nosplash -nodesktop -r "addpath('/home/matlab');addpath(genpath('/ndi'));addpath('/datain');ndi_from_dicom_v1('/datain','/datain/ndi_out/',0.1);exit;" | tail -n +11
+matlab -nodisplay -nosplash -nodesktop -r "addpath('/home/matlab');addpath(genpath('/ndi'));addpath('/datain');ndi_from_dicom_v1('/datain','/datain/ndi_out/',${intensity});exit;" | tail -n +11
